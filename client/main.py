177351aff4234e3db4fce81ab5ca2945bae46014
lgafurova <lgafurova@issart.com>
2024-07-09 16:26:27 +0600

    feat: prettify client

--- client/main.py
+++ client/main.py
@@ -4,6 +4,7 @@ import uuid
 import streamlit as st
 from dependency_injector import providers
 from pydash import find
+from streamlit.components.v1 import components
 
 from client.models import AccountReadModel, TransactionReadModel
 from core.dependencies import Container
@@ -16,6 +17,7 @@ from src.domain.account.queries import GetAllUserAccountsDTO, get_all_user_accou
 from src.domain.user.queries import get_user_by_id, GetUserDTO
 from tests.conftest import create_engine_for_tests
 
+st.set_page_config(layout="wide")
 st.title('Personal Finance')
 
 DB_USER_ID = '3ddf2e94-1eba-4079-8719-dc9fafa7edde'
@@ -35,7 +37,6 @@ container.wire(modules=[
 
 
 async def get_accounts_data(user):
-
     data: list[Account] = await get_all_user_accounts(GetAllUserAccountsDTO(user.id))
 
     display_data = [AccountReadModel(
@@ -48,7 +49,6 @@ async def get_accounts_data(user):
 
 
 async def get_transactions_data(user):
-    user = await get_user_by_id(GetUserDTO(id=uuid.UUID(DB_USER_ID)))
     txs = await get_user_transactions(GetUserTransactionsDTO(user.id))
     user_accounts = await get_all_user_accounts(GetAllUserAccountsDTO(user.id))
 
@@ -67,24 +67,7 @@ async def get_transactions_data(user):
     return display_data
 
 
-async def accounts_page(st):
-    user = await get_user_by_id(GetUserDTO(id=uuid.UUID(DB_USER_ID)))
-    with st.form('Add account'):
-        account_name = st.text_input('Account Name')
-        account_balance = st.number_input('Account Balance')
-
-        submitted = st.form_submit_button("Add")
-        if submitted:
-            await create_account(
-                CreateAccountDTO(
-                    user_id=uuid.UUID(DB_USER_ID),
-                    name=account_name,
-                    balance=account_balance
-                )
-            )
-    accounts_data = await get_accounts_data(user)
-    st.table(data=accounts_data)
-
+async def add_transaction__form(st, user):
     with st.form('Add Transaction'):
         from_number = st.text_input('From Account', value=None)
         to_number = st.text_input('To Account', value=None)
@@ -95,27 +78,59 @@ async def accounts_page(st):
             debit_account, credit_account = None, None
             if to_number:
                 debit_account = await get_account_by_number(GetAccountByNumberDTO(
-                    user_id=uuid.UUID(DB_USER_ID),
+                    user_id=user.id,
                     account_number=to_number
                 ))
             if from_number:
                 credit_account = await get_account_by_number(GetAccountByNumberDTO(
-                    user_id=uuid.UUID(DB_USER_ID),
+                    user_id=user.id,
                     account_number=from_number
                 ))
             await add_transaction_for_user(
                 AddTransactionDTO(
-                    user_id=uuid.UUID(DB_USER_ID),
+                    user_id=user.id,
                     credit_account_id=credit_account.id if credit_account else None,
                     debit_account_id=debit_account.id if debit_account else None,
                     amount=amount
                 )
             )
 
+
+async def add_account__form(st, user):
+    with st.form('Add account'):
+        account_name = st.text_input('Account Name')
+        account_balance = st.number_input('Account Balance')
+
+        submitted = st.form_submit_button("Add")
+        if submitted:
+            await create_account(
+                CreateAccountDTO(
+                    user_id=user.id,
+                    name=account_name,
+                    balance=account_balance
+                )
+            )
+
+
+async def accounts_page(st):
+    user = await get_user_by_id(GetUserDTO(id=uuid.UUID(DB_USER_ID)))
+
+    accounts_data = await get_accounts_data(user)
+    with st.container():
+        col1, col2 = st.columns(2)
+        with col1:
+            st.table(data=accounts_data)
+        with col2:
+            await add_account__form(st, user)
+
     txs_data = await get_transactions_data(user)
-    st.table(data=txs_data)
+    with st.container():
+        col1, col2 = st.columns(2)
+        with col1:
+            st.table(data=txs_data)
+        with col2:
+            await add_transaction__form(st, user)
 
 
 if __name__ == '__main__':
-
     asyncio.run(accounts_page(st))
